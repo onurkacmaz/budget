e1a10adfcde3531279e5eae13c34eba8b539ab86
Onur Kaçmaz <onur.kacmaz@path.com.tr>
2022-09-11 17:18:30 +0300

    some changes

--- database/migrations/2022_09_10_222840_add_phone_field_to_users.php
+++ database/migrations/2022_09_10_222840_add_phone_field_to_users.php
@@ -14,7 +14,7 @@ class AddPhoneFieldToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone');
+            $table->string('phone')->nullable();
         });
     }
 
