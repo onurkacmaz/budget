f64542220e5c89811c40dc0f2bcdca5a037ebb56
Onur Kaçmaz <onur.kacmaz@path.com.tr>
2022-09-11 17:21:21 +0300

    add phone param to request

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -24,6 +24,7 @@ class RegisterRequest extends BaseRequest
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users',
+            'phone' => 'required|numeric|unique:users',
             'password' => 'required|min:6|confirmed',
         ];
     }
